127de7e2a3534383837cc4a6768d8a626cfb6863
Jason Walker <jasonwalker15@gmail.com>
2026-02-26 16:37:23 -0600

    remove versioning buckets from s3

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,31 +2,10 @@ resource "aws_s3_bucket" "raw" {
   bucket = "${var.project_name}-raw"
 }
 
-resource "aws_s3_bucket_versioning" "raw" {
-  bucket = aws_s3_bucket.raw.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
 resource "aws_s3_bucket" "staging" {
   bucket = "${var.project_name}-staging"
 }
 
-resource "aws_s3_bucket_versioning" "staging" {
-  bucket = aws_s3_bucket.staging.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
 resource "aws_s3_bucket" "processed" {
   bucket = "${var.project_name}-processed"
-}
-
-resource "aws_s3_bucket_versioning" "processed" {
-  bucket = aws_s3_bucket.processed.id
-  versioning_configuration {
-    status = "Enabled"
-  }
 }
